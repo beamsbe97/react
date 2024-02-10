ec9ed1edcee57a0905c4d1c0064ce19c7e8ddc72
beamsbe97 <37875772+beamsbe97@users.noreply.github.com>
2024-02-11 00:54:29 +0800

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,25 +3,41 @@ import { useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Request from './Ting'
 
 
 
 function App() {
-  const refID = useRef()
+  const refID = useRef<any>()
+  const editID = useRef<any>()
+  const [editClick, setEditClick] = useState(false)
   const [click, setClick] = useState(false)
   const [reqList, setReqList] = useState(["1", "2", "3"])
 
   function updateList(){
-    setReqList([...reqList, {refID}])
-    
+    setReqList([...reqList, refID.current.value])
   }
-  
+
+  function ItemList(){
+    if(editClick==false){
+      return(<>{reqList.map(request => <li>{request} 
+      <button onClick={()=>{setEditClick(!editClick)}}>Edit</button> </li>)}
+      </>)
+    }
+    else{
+      return(<>{reqList.map(request => <li>{request} 
+        <input type="text"/>
+        <button onClick={()=>{setEditClick(!editClick)}}>Save</button> 
+        <button onClick={()=>{setEditClick(!editClick)}}>Cancel</button> 
+        </li>)}
+        </>)
+    }
+
+  }
 
   if(!click){
     return (
       <>
-        {reqList.map(request => <li>{request}</li>)}
+        <ItemList/>
         <button onClick={()=>{setClick(!click)}}>+</button>
       </>
     )
@@ -29,15 +45,15 @@ function App() {
   else{
     return (
       <>
-        {reqList.map(request => <li>{request}</li>)}
-        <label> Channel ID
+        <ItemList/>
+        <label> Channel ID 
           <input type="text" ref={refID}/>
         </label>
         <button onClick={updateList}> Save </button>
-      
+        <button onClick={()=>{setClick(!click)}}> Cancel </button>
       </>
     )
- ;
+ ; 
 }
 }
 
