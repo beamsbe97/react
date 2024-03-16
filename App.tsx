d1c97935c796932496a8d2ea36a6a49aea00176d
beamsbe97 <37875772+beamsbe97@users.noreply.github.com>
2024-03-16 15:48:32 +0800

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -6,54 +6,59 @@ import './App.css'
 
 
 
+
+
+function editList(index: number){
+  console.log(tempInput)
+  let edited_req = reqList.map((c,i)=>{
+    if (i===index){
+      return tempInput
+    }
+    else{
+      return c
+    }
+    })
+  setReqList(edited_req)
+}
+
+
+function updateList(){
+  setReqList([...reqList, refID.current.value])
+}
+
+function Item(){
+  //Need to decouple item states from item list
+}
+
+function ItemList(){
+  const [reqList, setReqList] = useState(["Utrecht", "Maastricht", "Nijmegen"])
+  
+  if(editClick==false){
+    return(<>{reqList.map((request,i) => <li key={i}>{request} 
+    <button onClick={()=>{setEditClick(!editClick)}}>Edit</button> </li>)}
+    </>)
+  }
+  else{
+    //Edit item menu
+    return(<>{reqList.map((request,i) => <li key={i}>{request} 
+      <input type="text" value={tempInput} onChange={e=>{setTempInput(e.target.value)}} autoFocus/>
+      <button onClick={()=>{editList(i)}}>Save</button> 
+      <button onClick={()=>{setEditClick(!editClick)}}>Cancel</button> 
+      </li>)}
+      </>)
+  }
+
+}
+
+
 function App() {
   const refID = useRef<any>()
   let editID = useRef<any>()
   const [tempInput, setTempInput] = useState<any>()
   const [editClick, setEditClick] = useState(false)
   const [click, setClick] = useState(false)
-  const [reqList, setReqList] = useState(["Utrecht", "Maastricht", "Nijmegen"])
+  
 
-
-  function editList(index: number){
-    console.log(tempInput)
-    let edited_req = reqList.map((c,i)=>{
-      if (i===index){
-        return tempInput
-      }
-      else{
-        return c
-      }
-     })
-    setReqList(edited_req)
-  }
-
-
-  function updateList(){
-    setReqList([...reqList, refID.current.value])
-  }
-
-  function Item(){
-    //Need to decouple item states from item list
-  }
-
-  function ItemList(){
-    if(editClick==false){
-      return(<>{reqList.map((request,i) => <li key={i}>{request} 
-      <button onClick={()=>{setEditClick(!editClick)}}>Edit</button> </li>)}
-      </>)
-    }
-    else{
-      //Edit item menu
-      return(<>{reqList.map((request,i) => <li key={i}>{request} 
-        <input type="text" value={tempInput} onChange={e=>{setTempInput(e.target.value)}} autoFocus/>
-        <button onClick={()=>{editList(i)}}>Save</button> 
-        <button onClick={()=>{setEditClick(!editClick)}}>Cancel</button> 
-        </li>)}
-        </>)
-    }
-
-  }
 
   if(!click){
     return (
