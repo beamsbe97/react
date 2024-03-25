70ac792741cb9e733eb10f427c3c897ac61c8009
beamsbe97 <37875772+beamsbe97@users.noreply.github.com>
2024-03-25 16:36:02 +0800

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -6,18 +6,20 @@ import './App.css'
 import Card from './Card.tsx'
 
 function App() {
-  const addID = useRef<any>()
-  const [reqList, setReqList] = useState(["Utrecht", "Maastricht", "Nijmegen"])
-  const [editClick, setEditClick] = useState(false)
-  const [addClick, setAddClick] = useState(false)
   const [addCardClick, setAddCardClick] = useState<boolean>(false)
-  const [editIndex, setEditIndex] = useState<number>()
-
   const [cardList, setCardList] = useState([{chid:123456, did: "B68S"}, 
                                             {chid:789420, did: "N58R"}])
   
   function newCard(childData: any){
     setCardList([...cardList, childData])
+  }
+
+  function renderAddBt(toRender: boolean){
+    setAddCardClick(toRender)
+  }
+
+  function deleteCard(index: number){
+    const tempList = cardList.filter((card, i)=> i!==index)
   }
 
   function cardEditSave(index: number, inputDID: string, inputID: any){
@@ -33,58 +35,14 @@ function App() {
     setCardList(tempList)
   }
 
-  function handleEditSave(index: number, userInput: string){
-    const tempList = reqList.map((request,i)=>{
-      if(i===index){
-        return userInput
-      }else{
-        return request
-      }
-    })
-    setEditClick(!editClick)
-    setReqList(tempList)
-  }
-
-  function handleEdit(index: number){
-    setEditClick(true)
-    setEditIndex(index)
-  }
-
-  function handleAdd(){
-    setAddClick(!addClick)
-    setReqList([...reqList, addID.current.value])
-  }
-
   return(
     <>
-      <>{reqList.map((request,i) => <li>{request}  
-      {(editClick && i===editIndex)?(
-        <>
-          <input type='text' id={i.toString()}></input>
-          <button onClick={()=>{handleEditSave(i,((document.getElementById(i.toString()) as HTMLInputElement).value))}}>Save</button>
-          <button onClick={()=>{setEditClick(!editClick)}}>Cancel</button>
-        </>
-      ):(
-        <button onClick={()=>{handleEdit(i)}}>Edit</button>
-      )}
-      </li>)}</>
-      {
-        addClick?(
-          <>
-            <input type="text" ref={addID}></input>  
-            <button onClick={()=>{handleAdd()}}>Save</button>  
-            <button onClick={()=>{setAddClick(!addClick)}}>Cancel</button>      
-          </>
-        ):(
-          <button onClick={()=>{setAddClick(true)}}>+</button>
-        )
-      }
-    <>{cardList.map((card,i) => <Card did={card.did} chid={card.chid.toString()} addNew={false} action={cardEditSave} cardIndex={i}/>)}</>
+    <>{cardList.map((card,i) => <Card did={card.did} chid={card.chid.toString()} addNew={false} action={cardEditSave} cardIndex={i} renderAdd={renderAddBt}/>)}</>
     {addCardClick?(
-      <> <Card did={""} chid={""} addNew={true} action={newCard} cardIndex=""/>
+      <> <Card did={""} chid={""} addNew={true} action={newCard} cardIndex="" renderAdd={renderAddBt}/>
       </>
     ):(
-      <><button onClick={()=>{setAddCardClick(!addCardClick)}}>Add Request</button></>
+      <><button className='button' onClick={()=>{setAddCardClick(!addCardClick)}}>Add Request</button></>
     )}
     
 
--- Card.tsx
+++ Card.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-function Card(props:{did:string, chid:string, addNew: boolean, action:any, cardIndex:any}){
+function Card(props:{did:string, chid:string, addNew: boolean, action:any, cardIndex:any, renderAdd:any}){
     const [editClick, setEditClick] = useState<boolean>(false)
     const [addClick, setAddClick] = useState<boolean>(props.addNew)
     const dataFields = ["Channel ID", "DID"]
@@ -10,46 +10,69 @@ function Card(props:{did:string, chid:string, addNew: boolean, action:any, cardI
         console.log(inputChID)
         console.log(inputDID)
         props.action({chid: inputChID.toString(), did:inputDID})
+        props.renderAdd(!addClick)
+        setAddClick(!addClick)
+    }
 
+    function handleNewCancel(){
+        props.renderAdd(!addClick)
         setAddClick(!addClick)
     }
 
     function handleEditSave(){
-        let inputDID = Math.max((document.getElementById("1") as HTMLInputElement).value.length, props.did.length)
-        let inputID = Math.max((document.getElementById("0") as HTMLInputElement).value.length, props.did.length)
+        let inputID = props.chid
+        let inputDID = props.did
+        if((document.getElementById("1") as HTMLInputElement).value.length>0){
+            inputDID = (document.getElementById("1") as HTMLInputElement).value
+        }
+
+        if((document.getElementById("0") as HTMLInputElement).value.length>0){
+            inputID = (document.getElementById("0") as HTMLInputElement).value
+        }
+    
         props.action(props.cardIndex, inputDID, inputID)
         setEditClick(!editClick)
     }
     return(addClick?(
         <>
         <div className="card">
-            <ul>
-            <label>New DID:<input type="text" id="inputDID"></input></label>
-            <label>New Channel ID:<input type="text" id="inputID"></input></label>
-            </ul>
-            <button onClick={()=>{handleNewSave(((document.getElementById("inputDID") as HTMLInputElement).value),
+            <ul><label>New DID:<input type="text" className="input" id="inputDID"></input></label></ul>
+            <ul><label>New Channel ID:<input type="text" className="input" id="inputID"></input></label></ul>
+            <button className="button" onClick={()=>{handleNewSave(((document.getElementById("inputDID") as HTMLInputElement).value),
                                                   ((document.getElementById("inputID") as HTMLInputElement).value))}}>Save</button>
-            <button onClick={()=>{setAddClick(!addClick)}}>Cancel</button></div>
+            <button className="button" onClick={()=>{handleNewCancel()}}>Cancel</button></div>
         </>
     ):(
         <>
+        {props.chid.length>0?(<>
         <div className="card">
-            
             {editClick?(
             <>
                 {dataFields.map((field,i)=><ul>{field.concat(": ", fieldVals[i])}
-                <input type="text" id={i.toString()}></input>
+                <input className="input" type="text" id={i.toString()}></input>
                 </ul>)}
-                <button onClick={()=>{setEditClick(!editClick)}}>Cancel</button>
-                <button onClick={()=>{handleEditSave()}}>Save</button>
+                <button className="button" onClick={()=>{handleEditSave()}}>Save</button>
+                <button className="button" onClick={()=>{setEditClick(!editClick)}}>Cancel</button>
+                <select>
+                    <option >Pending SPC</option>
+                    <option >Pending YE </option>
+                    <option >Confirmation Step</option>
+                    <option >Completed</option>
+                </select>
             </>
             ):(
             <>
                 {dataFields.map((field,i)=><ul>{field.concat(": ", fieldVals[i])}</ul>)}
-                <button onClick={()=>{setEditClick(!editClick)}}>Edit</button>
+                <button className="button" onClick={()=>{setEditClick(!editClick)}}>Edit</button>
             </>)}
 
         </div>
+        </>):(
+        <>
+        
+        
+        </>)}
+        
     </>))
 }
 
